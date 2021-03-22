3e776719d9dcbef4321c0c357fe5a93b7c4ce27b
Leon Bogaert <leon@tim-online.nl>
2021-03-22 17:41:02 +0100

    DateTime without timezone

--- json_types.go
+++ json_types.go
@@ -1,6 +1,9 @@
 package guestline
 
-import "time"
+import (
+	"encoding/xml"
+	"time"
+)
 
 type Date struct {
 	time.Time
@@ -12,4 +15,27 @@ func (d Date) MarshalSchema() string {
 
 type DateTime struct {
 	time.Time
+}
+
+func (t *DateTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	s := ""
+	err := d.DecodeElement(&s, &start)
+	if err != nil {
+		return err
+	}
+
+	if s == "" {
+		return nil
+	}
+
+	layout := "2006-01-02T15:04:05"
+	nt, err := time.Parse(layout, s)
+	*t = DateTime{Time: nt}
+	return err
+}
+
+func (t DateTime) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	layout := "2006-01-02T15:04:05"
+	s := t.Format(layout)
+	return e.EncodeElement(s, start)
 }
