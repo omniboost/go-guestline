02b87b5db73ae3e21b49dd3f5ed9c5f04508ab53
Leon Bogaert <leon@tim-online.nl>
2021-05-26 13:54:54 +0200

    Fix type

--- json_types.go
+++ json_types.go
@@ -13,6 +13,29 @@ func (d Date) MarshalSchema() string {
 	return d.Time.Format("2006-01-02")
 }
 
+func (t *Date) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	s := ""
+	err := d.DecodeElement(&s, &start)
+	if err != nil {
+		return err
+	}
+
+	if s == "" {
+		return nil
+	}
+
+	layout := "2006-01-02"
+	nt, err := time.Parse(layout, s)
+	*t = Date{Time: nt}
+	return err
+}
+
+func (t Date) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	layout := "2006-01-02"
+	s := t.Format(layout)
+	return e.EncodeElement(s, start)
+}
+
 type DateTime struct {
 	time.Time
 }
