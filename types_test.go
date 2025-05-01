3d6784f7d144913166665f41a3decd0811a04917
Leon Bogaert <leon@omniboost.io>
2025-05-01 09:37:17 +0200

    added test for XMLMapStringStruct

---
+++ types_test.go
@@ -0,0 +1,33 @@
+package guestline_test
+
+import (
+	"encoding/xml"
+	"testing"
+
+	"github.com/omniboost/go-guestline"
+)
+
+func TestXMLMapStringStructUnmarshalSlice(t *testing.T) {
+	b := `<root>
+		<header1>
+			<test>1</test>
+			<test>2</test>
+			<test>3</test>
+		</header1>
+	</root>`
+
+	target := struct {
+		guestline.XMLMapStringStruct[[]struct {
+			Test string `xml:"test"`
+		}]
+	}{}
+
+	err := xml.Unmarshal([]byte(b), &target)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(target.XMLMapStringStruct["header1"]) != 3 {
+        t.Errorf("expected 3, got %d", len(target.XMLMapStringStruct["header1"]))
+    }
+}
